8cb9bd1257ba9d4e16893ff578ae7f927e717188
argideritzalpea <crh2ke@virginia.edu>
2023-01-13 23:40:55 -0800

    Add test suite for Lab2

--- tsz/tsdb/skeletons/Index.lisp
+++ tsz/tsdb/skeletons/Index.lisp
@@ -22,6 +22,7 @@
 (
 ((:path . "matrix") (:content . "matrix: A test suite created automatically from the test sentences given in the Grammar Matrix questionnaire."))
 ((:path . "corpus") (:content . "IGT provided by the linguist"))
+((:path . "lab2") (:content . "IGT provided by the linguist"))
 ; New test suites here. For example:
 ; ((:path . "new-test-suite") (:content . "New Test Suite: A description of the new test suite located at the subdirectory new-test-suite."))
 )
