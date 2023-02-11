1e0fc3512420869a80f46ac8f6577d5a4f7e2e47
Abhinav Patil <apatil.cs@gmail.com>
2023-02-10 23:22:19 -0800

    Lab 6 before tests/skeleton

--- tsdb/skeletons/Index.lisp
+++ tsdb/skeletons/Index.lisp
@@ -22,6 +22,7 @@
 (
 ((:path . "matrix") (:content . "matrix: A test suite created automatically from the test sentences given in the Grammar Matrix questionnaire."))
 ((:path . "corpus") (:content . "IGT provided by the linguist"))
+((:path . "lab6") (:content . "Lab 6"))
 ((:path . "lab5") (:content . "Lab 5"))
 ((:path . "lab4") (:content . "Lab 4"))
 ((:path . "lab3") (:content . "Lab 3"))
